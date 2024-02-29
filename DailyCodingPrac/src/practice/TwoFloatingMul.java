7c66ba8f789c3065aa44b4bce2a1adb9ad021952
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-02-29 11:53:30 +0530

    //W A P for two floating number Note : Here I take input from user with the help of scanner        Then i use nextFloat() to store the floating value        I use double if my user want more accuracy

---
+++ DailyCodingPrac/src/practice/TwoFloatingMul.java
@@ -0,0 +1,18 @@
+package practice;
+import java.util.*;
+public class TwoFloatingMul {
+
+	public static void main(String[] args) {
+	float a;
+	float b;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the First Floating Integrals");
+	a = sc.nextFloat();
+	System.out.println("Enter the Second Floating Integrals");
+	b = sc.nextFloat();
+	double c = a * b;
+	System.out.println("Multiplication is" + c);
+	
+	}
+
+}
