d20b8e072c2aa63e5b8d1d21a61227b1e190f581
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-02-29 12:16:29 +0530

    //Swapping: storing input from user in another variable to swap the number.

---
+++ DailyCodingPrac/src/practice/SwappingTwoNo.java
@@ -0,0 +1,19 @@
+//W A P to swap two numbers
+package practice;
+import java.util.*;
+public class SwappingTwoNo {
+	public static void main(String [] args){
+        int a, b, c, d;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first no:");
+        a = sc.nextInt();
+        System.out.println("Enter the second no:");
+        b = sc.nextInt();
+        c = b;
+        d = a;
+        System.out.println("Swapped number is :");
+        System.out.println(c);
+        System.out.println(d);
+        
+    }
+}
