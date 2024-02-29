ed2776f5881d4c90bdf57a049bdb17f1083ab124
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-02-29 11:29:34 +0530

    Hello World Program

---
+++ DailyCodingPrac/src/practice/IntegerSum.java
@@ -0,0 +1,12 @@
+//Hello World Program
+package practice;
+
+public class IntegerSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
