8bf65a19ea9cfa20662bd2f144e7655aa447ea35
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-02 03:04:05 +0530

    ENHANCED FOR LOOP ( For-each loop ) to make the task simple to print array element

---
+++ DailyCodingPrac/src/practice/printArrayElement.java
@@ -0,0 +1,14 @@
+package practice;
+import java.util.*;
+public class printArrayElement {
+
+	public static void main(String[] args) {
+		int[] x = {1,2,3,4,5,6};
+		for(int x1 : x) {
+			System.out.println(x1);
+		}
+		
+
+	}
+
+}
