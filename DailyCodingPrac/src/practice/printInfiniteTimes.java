c8db08c503bed0bc7821e33bc8627157bffe528d
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-02 02:40:25 +0530

    forLoopInfinitePrint

---
+++ DailyCodingPrac/src/practice/printInfiniteTimes.java
@@ -0,0 +1,15 @@
+/*
+ * with the help of for loop print Hello infinite time;
+ */
+package practice;
+import java.util.*;
+public class printInfiniteTimes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(;;) {
+		}
+
+	}
+
+}
