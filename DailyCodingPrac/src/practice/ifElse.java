2ee2103464eac22afbddce0d1b458865bacf6ef9
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-01 20:22:35 +0530

    iF-else
    Note : The argument to the statement will be only boolean type .
    If by mistake we try to provide any other type we will get compile
    time error.

---
+++ DailyCodingPrac/src/practice/ifElse.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class ifElse {
+
+	public static void main(String[] args) {
+		int x = 10;
+		if( x == 10) {
+			System.out.println("Hi");
+		}
+		else {
+			System.out.println("Hello");
+		}
+
+	}
+
+}
