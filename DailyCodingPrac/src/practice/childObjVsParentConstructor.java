7f5642635471b71a08d36456d02c233aee667794
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-06 21:17:45 +0530

    Child Object vs Parent Constructor
    Whenever we are creating child class object automatically parent
    constructor will be executed to perform initialization for the instance
    variable which are inherited from parents.

---
+++ DailyCodingPrac/src/practice/childObjVsParentConstructor.java
@@ -0,0 +1,62 @@
+//package practice;
+/*
+public class childObjVsParentConstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		class Person{
+			String name;
+			int age;
+			Person(String name, int age){
+				this.name = name;
+				this.age = age;
+			}
+			class Students extends Person{
+				int rollno;
+				int marks;
+				Students(String name, int age, int rollno, int marks){
+					super(name,age);
+					this.rollno = rollno;
+					this.marks = marks;
+					Student s = new Students("Abhinav", 22, 101, 80);
+				}
+			}
+		}
+
+	}
+
+}
+*/
+package practice;
+
+public class childObjVsParentConstructor {
+
+    public static void main(String[] args) {
+        Students s = new Students("Abhinav", 22, 101, 80);
+        System.out.println("Name: " + s.name);
+        System.out.println("Age: " + s.age);
+        System.out.println("Roll No: " + s.rollno);
+        System.out.println("Marks: " + s.marks);
+    }
+
+    static class Person {
+        String name;
+        int age;
+
+        Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    static class Students extends Person {
+        int rollno;
+        int marks;
+
+        Students(String name, int age, int rollno, int marks) {
+            super(name, age);
+            this.rollno = rollno;
+            this.marks = marks;
+        }
+    }
+}
