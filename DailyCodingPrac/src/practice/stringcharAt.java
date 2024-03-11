92b3a4da9ae8fbf38beb42ae2af473dcc6683909
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-12 02:06:27 +0530

    string .charAt()

---
+++ DailyCodingPrac/src/practice/stringcharAt.java
@@ -0,0 +1,17 @@
+//string chart()
+package practice;
+import java.util.*;
+public class stringcharAt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "ABHINAV";
+		System.out.println(name.charAt(5));
+		System.out.println(name.charAt(1));
+		System.out.println(name.charAt(3));
+		System.out.println(name.charAt(2));
+
+
+	}
+
+}
