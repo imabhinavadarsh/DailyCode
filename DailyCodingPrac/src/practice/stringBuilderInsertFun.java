7d4ea075367caf71e4efa9f033964400efa072dc
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-12 02:26:12 +0530

    StringBuilder .insert()

---
+++ DailyCodingPrac/src/practice/stringBuilderInsertFun.java
@@ -0,0 +1,14 @@
+//StringBuilder .insert()
+package practice;
+import java.util.*;
+public final class stringBuilderInsertFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder("Abhinv");
+		sb.insert(5, 'a');
+		System.out.println(sb);
+
+	}
+
+}
