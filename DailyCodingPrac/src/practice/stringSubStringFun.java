30914dad2463b3d9034e2511a790386b7ec4c0d6
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-12 02:03:44 +0530

    String .substring(beg index, end index)

---
+++ DailyCodingPrac/src/practice/stringSubStringFun.java
@@ -0,0 +1,15 @@
+//string .substring()
+package practice;
+import java.util.*;
+public class stringSubStringFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sentence = "Hello sir how are you";
+		String sub = sentence.substring(0,5);
+		System.out.println(sub);
+		System.out.println(sentence.substring(6, sentence.length()));
+
+	}
+
+}
