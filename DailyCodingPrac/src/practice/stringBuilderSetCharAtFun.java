fc6d562fc30672064fbe5d4d2f17e7a8b39b260c
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-12 02:22:08 +0530

    StringBuilder .setCharAt()

---
+++ DailyCodingPrac/src/practice/stringBuilderSetCharAtFun.java
@@ -0,0 +1,14 @@
+//StringBuilder .setCharAt()
+package practice;
+import java.util.*;
+public class stringBuilderSetCharAtFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder("Abhi");
+	    sb.setCharAt(0, 'B');
+	    System.out.println(sb);
+
+	}
+
+}
