dca3e648a1b636937c6c96e1b1348720ad35fcb7
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-12 02:48:41 +0530

    StringBuilder .delete(starting index, ending index)

---
+++ DailyCodingPrac/src/practice/stringBuilderDeleteFun.java
@@ -0,0 +1,13 @@
+//StringBuilder .delete()
+package practice;
+import java.util.*;
+public class stringBuilderDeleteFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder("AABHINAV");
+		sb.delete(0,2);
+		System.out.println(sb);
+	}
+
+}
