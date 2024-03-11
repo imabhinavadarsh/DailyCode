f20c33db2a89cb32ca4460024f8d3203e6c28e23
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-12 02:50:50 +0530

    StringBuilder .append()

---
+++ DailyCodingPrac/src/practice/stringBuilderAppendFun.java
@@ -0,0 +1,13 @@
+//StringBuilder append()
+package practice;
+import java.util.*;
+public class stringBuilderAppendFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder("Abhi");
+		sb.append("nav");
+		System.out.println(sb);
+	}
+
+}
