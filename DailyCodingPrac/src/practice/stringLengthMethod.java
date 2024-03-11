aedfa659b9a516136afaf5456e99acdf7d3a3c5a
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-12 01:59:32 +0530

    String .length()

---
+++ DailyCodingPrac/src/practice/stringLengthMethod.java
@@ -0,0 +1,13 @@
+//String .length()
+package practice;
+import java.util.*;
+public class stringLengthMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sentence = "My name is Abhinav";
+		System.out.println(sentence.length());
+		
+	}
+
+}
