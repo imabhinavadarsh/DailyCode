898a5256604012708b24a440460516014150f350
Abhinav Adarsh <abhinavadarsh25@gmail.com>
2024-03-12 02:18:58 +0530

    StringBuilder charAt()

---
+++ DailyCodingPrac/src/practice/stringBuilderCharAtFun.java
@@ -0,0 +1,12 @@
+//StringBuilder charAt()
+package practice;
+import java.util.*;
+public class stringBuilderCharAtFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder("Abhi");
+		System.out.println(sb.charAt(0));
+	}
+
+}
